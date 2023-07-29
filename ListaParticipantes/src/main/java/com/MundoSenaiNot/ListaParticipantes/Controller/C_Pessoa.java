070236713fd30eddbe0a8797961cc62985abf709
Delci de Col <delcidecol@gmail.com>
2023-07-28 22:07:30 -0300

    confecção dos usuario e longin

--- ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/Controller/C_Pessoa.java
+++ ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/Controller/C_Pessoa.java
@@ -2,12 +2,19 @@ package com.MundoSenaiNot.ListaParticipantes.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_Pessoa {
     @GetMapping("/")
-    public String landPage(){
+    public String landPage() {
         return "Login/Login";
     }
+        @PostMapping("/")
+        public String loginPessoa (@RequestParam("usuario") String usuario, @RequestParam("senha") String senha){
+            return "Home/home";
+        }
 
-}
+    }
+
