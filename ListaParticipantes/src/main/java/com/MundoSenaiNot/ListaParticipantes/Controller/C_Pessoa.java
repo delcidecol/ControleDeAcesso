2dd060c3c2218ff0f1318d2c34fc45bbe4dbe7b5
Delci de Col <delcidecol@gmail.com>
2023-07-31 21:38:50 -0300

    cadastro de visitante

--- ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/Controller/C_Pessoa.java
+++ ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/Controller/C_Pessoa.java
@@ -1,5 +1,4 @@
 package com.MundoSenaiNot.ListaParticipantes.Controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/ListaParticipantesApplication.java
+++ ListaParticipantes/src/main/java/com/MundoSenaiNot/ListaParticipantes/ListaParticipantesApplication.java
@@ -9,7 +9,6 @@ public class ListaParticipantesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ListaParticipantesApplication.class, args);
 
-
 	}
 
 }
